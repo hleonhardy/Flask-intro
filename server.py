696c8ef9bbcf3e32e53eb5f12b602faaaeb8527e
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-17 13:01:24 -0700

    Finished the dropdown bar further study compliments.

---
+++ server.py
@@ -0,0 +1,108 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+def loop_compliments(AWESOMENESS):
+    string = ""
+    for compliment in AWESOMENESS:
+      string = string + "<option value={}>{}</option>".format(compliment, compliment)
+    return string
+
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return "<!doctype html><html>Hi! This is the home page.<a href='http://localhost:5000/hello'>Take me to hello page</a></html>"
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person"><br>
+          Choose your compliment: <select name='compliment'>{loop_compliments}
+          </select>
+          <input type="submit" value="Submit">
+        </form>
+        <form action="/diss">
+          What's your name? <input type="text" name="person"><br>
+          Choose your diss: <select name='diss'>
+            <option value='sweaty'>Sweaty</option>
+            <option value='ugly'>Ugly</option>
+            <option value='not awesome'>Not Awesome</option>
+          </select>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """.format(loop_compliments = loop_compliments(AWESOMENESS))
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+    compliment = request.args.get("compliment")
+
+    # compliment = choice(AWESOMENESS)
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """.format(player=player, compliment=compliment)
+
+
+@app.route('/diss')
+def diss_person():
+    """Diss user by name."""
+
+    player = request.args.get("person")
+    diss = request.args.get("diss")
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Diss</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {diss}!
+      </body>
+    </html>
+    """.format(player=player, diss=diss)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True)
